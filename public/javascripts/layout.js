b1075e634955e8a208ec4b367495b2f3b53f29d8
SharingWorld <hapsoa@gmail.com>
2018-07-30 21:40:54 +0900

    update layout

---
+++ public/javascripts/layout.js
@@ -0,0 +1,10 @@
+const $mainTitle = $('.main-title');
+
+const imgWidth = $mainTitle.width();
+const imgHeight = $mainTitle.height();
+
+console.log(imgHeight);
+
+
+$($mainTitle).css('background', '#010101');
+
