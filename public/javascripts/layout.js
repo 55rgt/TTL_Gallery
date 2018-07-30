cfa7a055b718bed3c5a6a7c7031a61e44715130b
SharingSmile <hapsoa@gmail.com>
2018-07-31 00:15:25 +0900

    update

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,10 +1,2 @@
-const $mainTitle = $('.main-title');
 
-const imgWidth = $mainTitle.width();
-const imgHeight = $mainTitle.height();
-
-console.log(imgHeight);
-
-
-$($mainTitle).css('background', '#010101');
 
