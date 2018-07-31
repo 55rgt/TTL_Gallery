b1239b9d743a45981069daf0900812de0f8074a7
SharingWorld <hapsoa@gmail.com>
2018-07-31 12:10:29 +0900

    filter navigator update

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,2 +1,30 @@
+const filterNavigator = new function() {
+    const $window = $(window);
+    const $mainTitle = $('.main-title');
+    const $filterNavigator = $('.filter-navigator');
+
+    $window.scroll(function() {
+
+        const headerHeight = 56 + $mainTitle.height();
+
+        // console.log(headerHeight);
+        // console.log($window.scrollTop());
+
+        if ($window.scrollTop() < headerHeight) {
+            // 스크롤이 상위에 있을 때
+            $filterNavigator.css('position', 'static');
+        }
+        else {
+            // 스크롤이 하위에 있을 때
+            $filterNavigator.css(
+                {'position': 'fixed',
+                    'left' : 0,
+                    'top' : 0
+                });
+        }
+
+    });
+};
+
 
 
