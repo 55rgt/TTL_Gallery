487723568bbe80615034c503432d3df4ac27f9f9
SharingWorld <hapsoa@gmail.com>
2018-07-31 19:22:31 +0900

    update

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -5,6 +5,9 @@ const filterNavigator = new function() {
 
     let scrollPosition = 'up';
 
+    /**
+     * 홈페이지 스크롤 시 필터 네비바 제어
+     */
     $window.scroll(function() {
 
         const headerHeight = 56 + $mainTitle.height();
@@ -32,6 +35,9 @@ const filterNavigator = new function() {
 
     });
 
+    /**
+     * 홈페이지 가로로 줄일 시 필터 네비바 제어
+     */
     $window.on('resize', () => {
         const headerHeight = 56 + $mainTitle.height();
 
@@ -45,8 +51,40 @@ const filterNavigator = new function() {
                'width' : '100%'
             });
         }
-
     });
+
+    /**
+     * 검색창
+     */
+    const $searchInput = $('input.form-control.mr-sm-2');
+    const $tagZone = $('.tag-zone');
+
+    $searchInput.on('keyup', function(event) {
+
+        if (event.keyCode === 13) {
+            console.log('entered');
+
+            const template = `<div class='tag'>#${$searchInput.val()}</div>`;
+            console.log(template);
+            $tagZone.append(template);
+
+            $searchInput.val('');
+        }
+    });
+
+    /**
+     * 토글 스위치 버튼
+     */
+    const $toggleButton = $('label.switch > input');
+
+    $toggleButton.on('click', function() {
+        if ($toggleButton.is(':checked')) {
+            // 버튼이 오른쪽에 있을 때
+        }
+        else {
+            // 버튼이 왼쪽에 있을 때
+        }
+    })
 
 };
 
