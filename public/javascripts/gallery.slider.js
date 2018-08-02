801771c8ef9341e1348f766450c14ba6002f1cbf
Junyup Hong <junyup.hong@gmail.com>
2018-08-02 16:47:27 +0900

    add slider

---
+++ public/javascripts/gallery.slider.js
@@ -0,0 +1,122 @@
+
+const Slider = function ($root, min, max) {
+    const temp = $root.append(`
+        <div class="slider-zone">
+            <div class="bar">
+                <div class="circle left">
+                <div class="circle-number">0</div>
+</div>
+                <div class="circle right"><div class="circle-number">20</div></div>
+            </div>
+
+        </div>`);
+
+    const points = [];
+    let count;
+
+    const $bar = $(temp.find('.bar'));
+
+
+    const setPoints = function () {
+        count = max - min + 1;
+        for(let i = 0; i < count; i++) {
+            points.push($bar.position().left + ($bar.width()/count)*i);
+        }
+
+    };
+    setPoints();
+
+    //
+    // const getLeftValue = function () {
+    //     const $left = $('.circle:first');
+    //     for(let i = 0; i < points.length; i++) {
+    //         if($left.position().left + $bar.position().left - points[i] < 0) {
+    //             return i-1;
+    //         }
+    //     }
+    //     return points.length-1;
+    // };
+    // const getRightValue = () => {
+    //     const $right = $('.circle:last');
+    //     for(let i = 0; i < points.length; i++) {
+    //         if($right.position().left + $bar.position().left - points[i] < 0) {
+    //             return i-1;
+    //         }
+    //     }
+    //     return points.length-1;
+    //
+    // };
+
+
+    this.getLeftValue = function () {
+        console.log(points);
+        const $left = $('.circle:first');
+        for(let i = min; i < points.length; i++) {
+            console.log('aa', $left.position().left + $bar.position().left, points[i]);
+
+            if($left.position().left + $bar.position().left - points[i] <= 0) {
+                return i;
+            }
+        }
+    };
+    this.getRightValue = () => {
+        const $right = $('.circle:last');
+        for(let i = min; i < points.length; i++) {
+            if($right.position().left + $bar.position().left - points[i] === 0) {
+                return i;
+            }
+            else if($right.position().left + $bar.position().left - points[i] < 0) {
+                return i;
+            }
+        }
+
+    };
+
+    const $circle = $(temp.find('.circle'));
+    $circle.on('mousedown', function () {
+        const dragItem = $(this);
+        dragItem.attr('id', 'drag');
+
+
+        dragItem.on('mousemove', function (event) {
+            if(event.clientX > $bar.position().left && event.clientX < $bar.position().left + $bar.width()) {
+                $(this).css('left', `${event.clientX - $circle.width()/2 - $bar.position().left}px`);
+            }
+        });
+
+    });
+
+
+
+    $(document).on('mouseup', function (event) {
+        const $drag = $('#drag');
+        $drag.off('mousemove');
+        $drag.attr('id', '');
+
+        const mouseX = event.clientX;
+
+        if(mouseX < points[0]) {
+            $drag.css('left', `${points[0] - $bar.position().left + $bar.width()/(count*2) -$circle.width()/2}px`);
+            $drag.find('.circle-number').text(`0`);
+
+        }
+
+        else if(mouseX > points[0] && mouseX < points[points.length-1]) {
+            for(let i = 0; i< points.length-1; i++) {
+                if(mouseX > points[i] && mouseX < points[i+1]) {
+                    $drag.css('left', `${points[i] - $bar.position().left + $bar.width()/(count*2) -$circle.width()/2}px`);
+                    $drag.find('.circle-number').text(`${i}`);
+                }
+            }
+        }
+        else {
+            $drag.css('left', `${points[points.length-1] - $bar.position().left + $bar.width()/(count*2) -$circle.width()/2}px`);
+            $drag.find('.circle-number').text(`${points.length-1}`);
+
+        }
+
+    });
+
+
+    return this;
+};
--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,164 +1,177 @@
 const filterNavigator = new function () {
-  const $window = $(window);
-  const $mainTitle = $('.main-title');
-  const $filterNavigator = $('.filter-navigator');
-  const $gallery = $('.gallery');
+    const $window = $(window);
+    const $mainTitle = $('.main-title');
+    const $filterNavigator = $('.filter-navigator');
+    const $gallery = $('.gallery');
 
-  let scrollPosition = 'up';
+    let scrollPosition = 'up';
 
-  /**
-   * 홈페이지 스크롤 시 필터 네비바 제어
-   */
-  $window.scroll(function () {
+    /**
+     * 홈페이지 스크롤 시 필터 네비바 제어
+     */
+    $window.scroll(function () {
 
-    const headerHeight = $mainTitle.height();
+        const headerHeight = $mainTitle.height();
 
-    if ($window.scrollTop() < headerHeight) {
-      // 스크롤이 상위에 있을 때
-      if (scrollPosition === 'down') {
-        $filterNavigator.css('position', 'static');
-          $gallery.css("margin-top", "0");
+        if ($window.scrollTop() < headerHeight) {
+            // 스크롤이 상위에 있을 때
+            if (scrollPosition === 'down') {
+                $filterNavigator.css('position', 'static');
+                $gallery.css("margin-top", "0");
 
-        scrollPosition = 'up';
-      }
-    }
-    else {
-      // 스크롤이 하위에 있을 때
-      if (scrollPosition === 'up') {
-        $filterNavigator.css(
-          {
-            'position': 'fixed',
-            'top': 0,
-            'width': $filterNavigator.parent().width()
-          });
-        $gallery.css("margin-top", `${$filterNavigator.height()}px`);
-        console.log($filterNavigator.height());
-        scrollPosition = 'down';
-      }
-    }
+                scrollPosition = 'up';
+            }
+        }
+        else {
+            // 스크롤이 하위에 있을 때
+            if (scrollPosition === 'up') {
+                $filterNavigator.css(
+                    {
+                        'position': 'fixed',
+                        'top': 0,
+                        'width': $filterNavigator.parent().width()
+                    });
+                $gallery.css("margin-top", `${$filterNavigator.height()}px`);
+                console.log($filterNavigator.height());
+                scrollPosition = 'down';
+            }
+        }
 
-  });
+    });
 
-  /**
-   * 홈페이지 가로로 줄일 시 필터 네비바 제어
-   */
-  $window.on('resize', () => {
-    const headerHeight = 56 + $mainTitle.height();
+    /**
+     * 홈페이지 가로로 줄일 시 필터 네비바 제어
+     */
+    $window.on('resize', () => {
+        const headerHeight = 56 + $mainTitle.height();
 
-    if ($window.scrollTop() > headerHeight) {
-      $filterNavigator.css({
-        'width': $filterNavigator.parent().width()
-      });
-    }
-    else {
-      $filterNavigator.css({
-        'width': '100%'
-      });
-    }
-  });
-
-  /**
-   * 검색창
-   */
-  const $searchInput = $('input.form-control.mr-sm-2');
-  const $minInput = $('input.min');
-  const $maxInput = $('input.max');
-  const $tagZone = $('.tag-zone');
-  const tagArray = [];
+        if ($window.scrollTop() > headerHeight) {
+            $filterNavigator.css({
+                'width': $filterNavigator.parent().width()
+            });
+        }
+        else {
+            $filterNavigator.css({
+                'width': '100%'
+            });
+        }
+    });
 
 
-  $.getJSON('../data/gallery.json', function (data) {
-
-    // 클릭이나 검색을 하면 -> 필터
-    let filterReceiver = new FilterReceiver(data);
-    $searchInput.on('keyup', function (event) {
 
 
-      if (event.keyCode === 13 && $searchInput.val().trim() !== '' &&
-        !tagArray.includes($searchInput.val().trim()) && tagArray.length < 6) {
-
-        const template = `<div class='tag'>${$searchInput.val().trim()}</div>`;
-
-        $tagZone.append(template);
-
-        tagArray.push($searchInput.val().trim());
-
-        filterReceiver.setTags(tagArray);
-        console.log(filterReceiver.getTags());
-
-        filterReceiver.setValue(parseInt($minInput.val()), parseInt($maxInput.val()));
-        filterReceiver.updateFilter();
-        console.log(filterReceiver.getFilteredData());
-
-        $searchInput.val('');
-
-        $('.main-mode').empty();
-        $('.list-mode').empty();
-        dataLayout.setFile(filterReceiver.getFilteredData());
+    /**
+     * scroll-filter
+     */
+    const $scrollZone = $('.scroll-filter');
+    const min = 0;
+    const max = 20;
+    const slider = new Slider($scrollZone, min, max);
 
 
-        /**
-         * 태그들
-         */
-        const $tags = $('.tag');
 
-        $tags.unbind();
-
-        $tags.on('click', function () {
-          const $this = $(this);
-
-          tagArray.splice(tagArray.indexOf($this.text()), 1);
-          console.log(tagArray);
-          $this.remove();
-
-          if (tagArray.length === 0) filterReceiver.getFilter().resetData();
-          else filterReceiver.getFilter().filterByTagNames(tagArray, false);
-          // console.log(filterReceiver.getFilter().getCurrentData());
+    /**
+     * 검색창
+     */
+    const $searchInput = $('input.form-control.mr-sm-2');
+    const $minInput = $('input.min');
+    const $maxInput = $('input.max');
+    const $tagZone = $('.tag-zone');
+    const tagArray = [];
 
 
-          $('.main-mode').empty();
-          $('.list-mode').empty();
-          dataLayout.setFile(filterReceiver.getFilter().getCurrentData());
+    $.getJSON('../data/gallery.json', function (data) {
 
+        // 클릭이나 검색을 하면 -> 필터
+        let filterReceiver = new FilterReceiver(data);
+        $searchInput.on('keyup', function (event) {
+
+
+            if (event.keyCode === 13 && $searchInput.val().trim() !== '' &&
+                !tagArray.includes($searchInput.val().trim()) && tagArray.length < 6) {
+
+                const template = `<div class='tag'>${$searchInput.val().trim()}</div>`;
+
+                $tagZone.append(template);
+
+                tagArray.push($searchInput.val().trim());
+
+                filterReceiver.setTags(tagArray);
+                console.log(filterReceiver.getTags());
+
+                // filterReceiver.setValue(parseInt($minInput.val()), parseInt($maxInput.val()));
+                filterReceiver.setValue(slider.getLeftValue(), slider.getRightValue());
+                filterReceiver.updateFilter();
+                console.log(filterReceiver.getFilteredData());
+
+                $searchInput.val('');
+
+                $('.main-mode').empty();
+                $('.list-mode').empty();
+                dataLayout.setFile(filterReceiver.getFilteredData());
+
+
+                /**
+                 * 태그들
+                 */
+                const $tags = $('.tag');
+
+                $tags.unbind();
+
+                $tags.on('click', function () {
+                    const $this = $(this);
+
+                    tagArray.splice(tagArray.indexOf($this.text()), 1);
+                    console.log(tagArray);
+                    $this.remove();
+
+                    if (tagArray.length === 0) filterReceiver.getFilter().resetData();
+                    else filterReceiver.updateFilter();
+                    // console.log(filterReceiver.getFilter().getCurrentData());
+
+
+                    $('.main-mode').empty();
+                    $('.list-mode').empty();
+                    dataLayout.setFile(filterReceiver.getFilter().getCurrentData());
+
+                });
+            }
         });
-      }
-    });
-  });
-
-  /**
-   * 토글 스위치 버튼
-   */
-  const $toggleButton = $('label.switch > input');
-  const $pinterest = $('.main-mode');
-  const $list = $('.list-mode');
-
-  $toggleButton.on('click', function () {
-    if ($toggleButton.is(':checked')) {
-
-      if (!$pinterest.hasClass('display-none'))
-        $pinterest.addClass('display-none');
-
-      if ($list.hasClass('display-none'))
-        $list.removeClass('display-none');
-    }
-    else {
-
-      if (!$list.hasClass('display-none'))
-        $list.addClass('display-none');
-
-      if ($pinterest.hasClass('display-none'))
-        $pinterest.removeClass('display-none');
-    }
-
-    $('.main-mode').imagesLoaded().progress(function () {
-      $('.main-mode').masonry('layout');
     });
 
-  });
+    /**
+     * 토글 스위치 버튼
+     */
+    const $toggleButton = $('label.switch > input');
+    const $pinterest = $('.main-mode');
+    const $list = $('.list-mode');
+
+    $toggleButton.on('click', function () {
+        if ($toggleButton.is(':checked')) {
+
+            if (!$pinterest.hasClass('display-none'))
+                $pinterest.addClass('display-none');
+
+            if ($list.hasClass('display-none'))
+                $list.removeClass('display-none');
+        }
+        else {
+
+            if (!$list.hasClass('display-none'))
+                $list.addClass('display-none');
+
+            if ($pinterest.hasClass('display-none'))
+                $pinterest.removeClass('display-none');
+        }
+
+        $('.main-mode').imagesLoaded().progress(function () {
+            $('.main-mode').masonry('layout');
+        });
+
+    });
 
 
 };
-
 
 
 var swiper = new Swiper('.swiper-container', {
